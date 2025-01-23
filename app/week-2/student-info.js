e07d088c7b561ed21a985bab8b4ffd218208e0bf
Dafisto <166645358+Dafisto@users.noreply.github.com>
2025-01-23 09:41:59 -0700

    migrated to new repo
    -new repo created for proper root directory

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+    return (
+    <main>
+        <h1>Name: Chris Reid</h1>
+        <p><Link href="https://github.com/Dafisto/cprg306-assignments.git">Go to Repository</Link></p>
+    </main>
+    );
+}
