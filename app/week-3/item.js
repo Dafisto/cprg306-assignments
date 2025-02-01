bc5476ec2d01fe9fbe21a1e593570a8ba865d56e
Dafisto <166645358+Dafisto@users.noreply.github.com>
2025-02-01 11:07:22 -0700

    -adjustments to tailwind
    -used flex to re-arranged items in sections by row and column.

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -3,12 +3,10 @@ export default function Item(props){
     
 
 return(
-    <div className="w-25 h-25">
-        <ol className="bg-green-800 border-2 border-blue-800 px-4 py-5 mx-5 my-10">
-            <li className="font-bold text-black">Name: {props.name}</li>
-            <li className="text-red-600">Quantity: {props.quantity}</li>
-            <li className="text-blue-400">Category: {props.category}</li>
-        </ol>    
-    </div>
+    <ol className="flex-1 bg-green-800 border-2 border-blue-800 px-4 py-5 mx-5 my-7">
+        <li className="font-bold text-black">Name: {props.name}</li>
+        <li className="text-red-600">Quantity: {props.quantity}</li>
+        <li className="text-blue-400">Category: {props.category}</li>
+    </ol>
     );
 }
--- app/week-3/page.js
+++ app/week-3/page.js
@@ -3,8 +3,8 @@ import ItemList from "./item-list.js"
 export default function Page(){
 return(
         <main>
-            <h1 className="border-4 border-blue-800 px-2 mx-2 py-4 my-4 text-xl font-bold text-center text-red-300 bg-slate-800">Shopping List</h1>            
-            <ItemList className="flex-1" />
+            <h1 className="border-4 border-blue-800 px-10 mx-2 py-10 my-4 text-xl font-bold text-center text-red-300 bg-slate-800">Shopping List</h1>
+            <ItemList />
         </main>
     );
 }
